05a1c04f767817655c1a921f27fa8c18ef355d4d
Aryan Gupta <118144312+agupt295@users.noreply.github.com>
2024-12-07 13:55:37 -0600

    Update index.js

--- project/src/index.js
+++ project/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Include Font Awesome
+const fontAwesomeLink = document.createElement("link");
+fontAwesomeLink.rel = "stylesheet";
+fontAwesomeLink.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css";
+document.head.appendChild(fontAwesomeLink);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
