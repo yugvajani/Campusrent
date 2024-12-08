02f0c6f3a57c5aa0383fbb95a862ac1d77b03d09
Aryan Gupta <aryang10@illinois.edu>
2024-12-07 22:27:24 -0600

    fixed home-page routes

--- project/src/App.js
+++ project/src/App.js
@@ -8,7 +8,6 @@ import NewPasswordPage from "./Pages/ForgotPassword/NewPasswordPage";
 import NewLendPage from "./Pages/NewLendItem";
 import HomePage from "./Pages/HomePage";
 import ProfilePage from "./Pages/ProfilePage";
-import Home from "./Pages/Home";
 
 function App() {
   return (
@@ -16,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/verification" element={<VerificationCodePage />} />
